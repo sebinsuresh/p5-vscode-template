a1890ee68eb5489b508ec64ad02a0d0554eaf031
Sebin Suresh <sebinsuresh@gmail.com>
2024-06-08 14:22:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions
+
+```sh
+npm i
+```
+
+Install & configure `nodemon` or use VSCode Live server extension to run the project
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  background("#000");
+  fill("#fff");
+  rectMode(CENTER);
+  square(width / 2, height / 2, 100);
+}
