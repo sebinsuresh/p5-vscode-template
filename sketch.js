3eb968373834b4874420bc49ac9a265657a2e8ec
Sebin Suresh <sebinsuresh@gmail.com>
2024-06-08 17:24:11 -0500

    Change some code

--- sketch.js
+++ sketch.js
@@ -1,10 +1,73 @@
+/**
+ * @typedef { {x: number;y: number;} } Point
+ */
+
+const doubleClickDelay = 250;
+/** @type { Point | undefined }*/
+let previousPoint = undefined;
+let previousTime = 0;
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(windowWidth, windowHeight);
+  cursor("pointer");
 }
 
 function draw() {
+  clearCanvas();
+  drawCorners();
+
+  if (previousPoint) {
+    stroke("#aaa");
+    strokeWeight(2);
+    line(previousPoint.x, previousPoint.y, mouseX, mouseY);
+    strokeWeight(16);
+    point(previousPoint.x, previousPoint.y);
+
+    angleMode(DEGREES);
+    const yDiff = mouseY - previousPoint.y;
+    const xDiff = mouseX - previousPoint.x; // What if xDiff is 0?
+    const atanAngle = ~~atan(yDiff / xDiff);
+    const atan2Angle = ~~atan2(yDiff, xDiff);
+    noStroke();
+    fill("#fff");
+    text(`atanAngle: ${atanAngle}`, 10, 30);
+    text(`atan2Angle: ${atan2Angle}`, 10, 50);
+
+    noStroke();
+    arc(previousPoint.x, previousPoint.y, 200, 200, atan2Angle, 0);
+  }
+
+  strokeWeight(16);
+  stroke("#fff");
+  point(mouseX, mouseY);
+}
+
+function clearCanvas() {
   background("#000");
-  fill("#fff");
-  rectMode(CENTER);
-  square(width / 2, height / 2, 100);
+}
+
+function drawCorners() {
+  stroke("#800");
+  strokeWeight(8);
+  line(0, 0, 0, 20);
+  line(0, 0, 20, 0);
+  line(width, 0, width, 20);
+  line(width, 0, width - 20, 0);
+  line(0, height, 0, height - 20);
+  line(0, height, 20, height);
+  line(width, height, width, height - 20);
+  line(width, height, width - 20, height);
+}
+
+function touchStarted() {
+  const clickInterval = millis() - previousTime;
+  if (!previousPoint || clickInterval < doubleClickDelay) {
+    previousPoint = { x: mouseX, y: mouseY };
+  } else {
+    previousTime = millis();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
